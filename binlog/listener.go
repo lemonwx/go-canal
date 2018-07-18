b6d7ec6cf5d731940b6939f3a9f335cc95239ec7
lim <wxgwmmoc@126.com>
2018-07-18 22:21:57 +0800

    add: send dump cmd & partly parse header and event

--- binlog/listener.go
+++ binlog/listener.go
@@ -6,33 +6,100 @@
 package binlog
 
 import (
-	"github.com/lemonwx/xsql/node"
+	"encoding/binary"
+
+	"github.com/juju/errors"
+	"github.com/lemonwx/log"
 	"github.com/lemonwx/xsql/mysql"
-	"fmt"
+	"github.com/lemonwx/xsql/node"
 )
 
 const (
 	DEFAULT_SCHEMA = "information_schema"
 )
 
-type BinlogDumper struct {
+type Dumper struct {
 	*node.Node
 }
 
-func NewBinlogDumper (host string, port int, user, password string) *BinlogDumper{
+func NewBinlogDumper(host string, port int, user, password string) *Dumper {
 	node := node.NewNode(host, port, user, password, DEFAULT_SCHEMA, 0)
-	err := node.Connect()
-	if err != nil {
-		fmt.Println(err)
-	}
-	return &BinlogDumper{node}
+	return &Dumper{node}
 }
 
-func (dumper *BinlogDumper) Start() {
-	ret, err := dumper.Execute(mysql.COM_QUERY, []byte("show master status"))
-	if err != nil {
-		fmt.Println(err)
-	}
-	fmt.Println(ret.FieldNames)
+func (dumper *Dumper) getFileAndPos() (string, uint32, error) {
+	return "mysql-bin.000001", 4, nil
 }
 
+func (dumper *Dumper) writeDumpCmd() error {
+
+	logName, logPos, err := dumper.getFileAndPos()
+	if err != nil {
+		return errors.Trace(err)
+	}
+
+	data := make([]byte, 4+1+4+2+4+len(logName))
+
+	pos := 4
+	data[pos] = mysql.COM_BINLOG_DUMP
+	pos++
+
+	binary.LittleEndian.PutUint32(data[pos:], logPos)
+	pos += 4
+
+	binary.LittleEndian.PutUint16(data[pos:], 0)
+	pos += 2
+
+	binary.LittleEndian.PutUint32(data[pos:], 123456789)
+	pos += 4
+
+	copy(data[pos:], logName)
+
+	dumper.SetPktSeq(0)
+	dumper.WritePacket(data)
+
+	return nil
+}
+
+func (dumper *Dumper) Init() error {
+
+	err := dumper.Connect()
+	if err != nil {
+		return errors.Trace(err)
+	}
+
+	_, err = dumper.Execute(mysql.COM_QUERY, []byte("set @master_binlog_checksum= @@global.binlog_checksum"))
+	if err != nil {
+		return errors.Trace(err)
+	}
+
+	if err = dumper.writeDumpCmd(); err != nil {
+		return errors.Trace(err)
+	}
+	return nil
+}
+
+func (dumper *Dumper) Start() error {
+
+	for {
+		pkt, err := dumper.ReadPacket()
+		if err != nil {
+			return errors.Trace(err)
+			break
+		}
+
+		switch pkt[0] {
+		case mysql.ERR_HEADER:
+			log.Debug(pkt[1:])
+		case mysql.OK_HEADER:
+			header := &EveHeader{}
+			header.Decode(pkt)
+			log.Debug(header.Dump())
+		}
+	}
+	return nil
+}
+
+func (Dumper *Dumper) parseEvent() (Event, error) {
+	return nil, nil
+}
--- cmd/main.go
+++ cmd/main.go
@@ -6,8 +6,11 @@
 package main
 
 import (
+	"os"
+
 	"github.com/lemonwx/go-canal/binlog"
 	"github.com/lemonwx/go-canal/server"
+	"github.com/lemonwx/log"
 )
 
 const (
@@ -24,7 +27,12 @@ const (
 func main() {
 	// binlog listener start
 	// server start
+	log.NewDefaultLogger(os.Stdout)
+	log.SetLevel(100)
 	dumper := binlog.NewBinlogDumper(host, port, user, password)
+	if err := dumper.Init(); err != nil {
+		log.Error(err)
+	}
 	dumper.Start()
 
 	svr, err := server.NewServer(svrHost, svrPort)
